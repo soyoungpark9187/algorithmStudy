31bf4b55aaa50f1c352e8fd22569da51cab175de
soyoung <44626132+soyoungpark9187@users.noreply.github.com>
2020-06-21 00:53:12 +0900

    Create sorting_k번째수.cpp

---
+++ sorting_k번째수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+bool compare (int a, int b) {
+    return a < b;
+}
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> original = array;
+    vector<int> middle;
+    for (int i=0; i<commands.size(); i++) {
+        array = original;
+        vector<int>::iterator iter = array.begin();        
+        middle.assign(array.begin()+commands.at(i).at(0)-1, array.begin()+commands.at(i).at(1));
+        sort(middle.begin(), middle.end(), compare);
+        answer.push_back(middle.at(commands.at(i).at(2)-1));
+    }
+    return answer;
+}
