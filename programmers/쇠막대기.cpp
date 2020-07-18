1fa0e82e3116d2100046b17b549c6fb7aae06406
soyoung <44626132+soyoungpark9187@users.noreply.github.com>
2020-07-19 04:03:59 +0900

    Update 쇠막대기.cpp

--- programmers/쇠막대기.cpp
+++ programmers/쇠막대기.cpp
@@ -1,52 +1,24 @@
+// 모범답안
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <stack>
+
 using namespace std;
 
-int solution(string arrangement) {
+int solution(string a) {
     int answer = 0;
-    int toInt[arrangement.length()];
-    int sticks[100000] ={0, };
-    if (arrangement[1] == ')')
-        toInt[0] = -1;
-    else
-        toInt[0] = 2;
-    for (int i=1; i<arrangement.length(); i++) {
-        if (arrangement[i] ==')' && arrangement[i-1]== '(') {
-                toInt[i-1] = -1;
-                toInt[i] = -2;
-        }
-        else {
-            if (arrangement[i] == '(')
-                toInt[i] = 2;
+    stack<char> s;
+
+    for(int i=0 ; i<a.length();i++){
+        if(a[i] =='(')
+            s.push(a[i]);
+        else{
+            s.pop();
+            if(a[i-1] =='(')
+                answer += s.size();
             else
-                toInt[i] = 3;
+                answer ++;
         }
     }
-    for (int i=arrangement.length()-1; i>=0; i--) {
-        if (toInt[i] == 2) {
-            answer+=1;
-            int min=arrangement.length();
-            int min_j=1;
-            for (int j=i; j<arrangement.length(); j++) {
-                if (toInt[j]==3) {
-                    if (min > j-i){
-                        min=j-i;
-                        min_j=j;
-                    }
-                }
-            }
-            toInt[min_j] = 4;
-            toInt[i] = 4;
-            for (int x=i; x<min_j; x++) 
-                sticks[x]++;   
-            }    
-    }
-    for (int i=0; i<arrangement.length(); i++) {
-        if (sticks[i]!=0 && toInt[i] == -1) {
-            answer+=sticks[i];
-        }
-    }
-    
     return answer;
 }
