086cea5c198509070024388d832ce6253b8c37d8
soyoung <44626132+soyoungpark9187@users.noreply.github.com>
2020-08-05 04:24:54 +0900

    Update 위장.cpp
    unordered_map

--- programmers/위장.cpp
+++ programmers/위장.cpp
@@ -1,29 +1,20 @@
 #include <string>
 #include <vector>
 
+#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
-    vector<string> cat;
-    int len[30] = {0,};
-    int cnt=0;
+    unordered_map<string, int> m;
     
     for (int i=0; i<clothes.size(); i++) {
-        if (find(cat.begin(), cat.end(), clothes.at(i)[1]) != cat.end()) {
-           len[distance(cat.begin(), find(cat.begin(), cat.end(), clothes.at(i)[1]))]++;            
-        }
-        else {
-            cat.push_back(clothes.at(i)[1]);
-            len[cnt]++;  
-            cnt++;
-        }
+        m[clothes.at(i)[1]]++;
     }
-    for (int i=0; i < cnt; i++) {
-        answer *= (len[i]+1);
-    }
+    for (auto i=m.begin(); i!=m.end(); i++)
+        answer*=(i->second) + 1;
     answer-=1;
     return answer;
 }
