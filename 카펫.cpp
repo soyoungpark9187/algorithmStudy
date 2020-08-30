3f09e4200c732c7f8abdf772747ef59904f60d6d
soyoung <44626132+soyoungpark9187@users.noreply.github.com>
2020-08-31 02:15:41 +0900

    Create 카펫.cpp

---
+++ 카펫.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    int mul = brown+yellow;
+    int s = brown/2 + 2;
+    printf("%d %d\n", mul, s);
+    for (int i=1; i<=int(mul/2); i++) {
+        if (mul % i == 0) {
+            printf("%d\n", i);
+            if (mul % (s-i) == 0) {
+                if (i * (s-i) == mul){
+                if ( i > s-i) {
+                    answer.push_back(i);
+                    answer.push_back(s-i);
+                }
+                else {
+                    answer.push_back(s-i);
+                    answer.push_back(i);
+                }
+                break;
+                }
+            }
+        }
+    }
+    return answer;
+}
