4402acfe0d11b97b9e485220a3d616b53ef7b925
soyoung <44626132+soyoungpark9187@users.noreply.github.com>
2020-10-12 00:29:07 +0900

    Update 특정거리의도시찾기.cpp
    queue 사용

--- baekjoon/특정거리의도시찾기.cpp
+++ baekjoon/특정거리의도시찾기.cpp
@@ -4,24 +4,27 @@ using namespace std;
 int main() {
   int n, m, k, x; 
   cin >> n >> m >> k >> x;
-  vector<pair<int, int>> v;
+  vector<int> v[300001];
   vector<int> c(300001, -1);
   int n1, n2;
   for (int i=0; i<m; i++) {
     cin >> n1 >> n2;
-    v.push_back({n1, n2});
-    if (n1 == x) c[n2] = 1;
+    v[n1].push_back(n2);
   }
   c[x] = 0;
-  for (int i=1; i<k; i++) {
-    for (int j=0; j<m; j++) {
-      for (int t=0; t<m; t++) {
-        if (v[j].second == v[t].first && v[j].first == x) {
-          if (c[v[t].second] == -1) c[v[t].second] = c[v[j].second] + 1;
+  queue<int> q;
+    q.push(x);
+    while (!q.empty()) {
+        int now = q.front();
+        q.pop();
+        for (int i = 0; i <  v[now].size(); i++) {
+            int next = v[now][i];
+            if (c[next] == -1) {
+                c[next] = c[now] + 1;
+                q.push(next);
+            }
         }
-      }
     }
-  }
   int cnt = 0;
   for (int i=1; i<=n; i++) {
     if (c[i] == k) {
