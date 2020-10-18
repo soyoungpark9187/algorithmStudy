2a8784a3986e3718a33b336b23f6eed392136eae
soyoung <44626132+soyoungpark9187@users.noreply.github.com>
2020-10-18 15:15:24 +0900

    Create 피보나치수열.cpp

---
+++ wind/피보나치수열.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fibo (int n) {
+  if (n == 1 || n == 2) return 1;
+  else return fibo(n-1) + fibo(n-2);
+}
+int main() {
+  int n;
+  cin >> n;
+  cout << fibo(n) << "\n";
+}
