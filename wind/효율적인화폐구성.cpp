f5460b7c52fcddf2b6679374088c34211a6eb6be
soyoung <44626132+soyoungpark9187@users.noreply.github.com>
2020-10-18 16:55:11 +0900

    Update 효율적인화폐구성.cpp

--- wind/효율적인화폐구성.cpp
+++ wind/효율적인화폐구성.cpp
@@ -5,23 +5,20 @@ int main() {
   int n, m;
   cin >> n >> m;
   int arr[100];
-  int d[100] = {0, };
+  vector<int> d(m+1, 10001);
   for (int i=0; i<n; i++) {
    cin >> arr[i];
    d[arr[i]] = 1;
   }
   for (int i=1; i<=m; i++) {
-    if (d[i] == 0) {
-      d[i] = 10000;
-      for (int j=0; j<n; j++) {
-        if (i > arr[j]) {
-          if (d[i-arr[j]] != 0) {
-            d[i] = min(d[i], d[i-arr[j]] + 1);
-          }
+    for (int j=0; j<n; j++) {
+      if (i > arr[j]) {
+        if (d[i-arr[j]] != 0) {
+          d[i] = min(d[i], d[i-arr[j]] + 1);
         }
       }
     }
   }
-  if (d[m] == 10000) cout << "-1" << "\n";
+  if (d[m] == 10001) cout << "-1" << "\n";
   else  cout << d[m] << "\n";
 }
