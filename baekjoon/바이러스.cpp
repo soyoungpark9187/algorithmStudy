39ade234e6e67176f1df581cf199ecd143c18cc5
soyoung <44626132+soyoungpark9187@users.noreply.github.com>
2020-11-13 19:09:22 +0900

    Create 바이러스.cpp

---
+++ baekjoon/바이러스.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, c;
+  cin >> n;
+  cin >> c;
+  vector<int> v[102];
+  for (int i; i<c; i++) {
+    int f, t;
+    cin >> f >> t;
+    v[f].push_back(t);
+    v[t].push_back(f);
+  }
+  vector<int> virused;
+  queue<int> iter;
+  int checked[101];
+  iter.push(1);
+  virused.push_back(1);
+  checked[1] = 1;
+  int it = 0;
+  while (!iter.empty()) {
+    for (int i=0; i< v[iter.front()].size(); i++) {
+      int to = v[iter.front()][i];
+      virused.push_back(to);
+      if (checked[to] != 1)
+        iter.push(to);
+      checked[to] = 1;
+    }
+    it++;
+    iter.pop();
+  }
+  sort(virused.begin(), virused.end());
+  virused.erase(unique(virused.begin(),virused.end()),virused.end());
+  cout << virused.size()-1 << "\n";
+}
